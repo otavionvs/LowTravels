29d69c7020604988d3d8a5bef32a1b74bd3e6dcd
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2022-10-19 19:27:50 -0300

    Initial commit

---
+++ LowTravels/src/main/java/br/low/travels/LowTravels/LowTravelsApplication.java
@@ -0,0 +1,13 @@
+package br.low.travels.LowTravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowTravelsApplication.class, args);
+	}
+
+}
---
+++ LowTravels/src/test/java/br/low/travels/LowTravels/LowTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.low.travels.LowTravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
