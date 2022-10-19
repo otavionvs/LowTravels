6faa5bd8a12213f603e65112d0e1745e0fd6cbc2
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2022-10-19 20:05:08 -0300

    Cliente Ok

--- LowTravels/src/main/java/br/low/travels/LowTravels/dto/ClienteDTO.java
+++ LowTravels/src/main/java/br/low/travels/LowTravels/dto/ClienteDTO.java
@@ -1,4 +1,23 @@
 package br.low.travels.LowTravels.dto;
 
+import br.low.travels.LowTravels.model.entity.Genero;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class ClienteDTO {
+    //    @NotNull
+    private Long cpf;
+    //    @NotNull
+    private String nome;
+    //    @NotNull
+    private String sobrenome;
+    //    @NotNull
+    private String email;
+    //    @NotNull
+    private String senha;
+    //    @NotNull
+    private Genero genero;
+
 }
--- LowTravels/src/main/java/br/low/travels/LowTravels/model/entity/Cliente.java
+++ LowTravels/src/main/java/br/low/travels/LowTravels/model/entity/Cliente.java
@@ -1,4 +1,35 @@
 package br.low.travels.LowTravels.model.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pessoa")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Cliente {
+    @Id
+    @Column(length = 11, nullable = false, unique = true)
+    private Long cpf;
+
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    @Column(length = 75, nullable = false)
+    private String sobrenome;
+
+    @Column(length = 125, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 25, nullable = false)
+    private String senha;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 15, nullable = false)
+    private Genero genero;
 }
