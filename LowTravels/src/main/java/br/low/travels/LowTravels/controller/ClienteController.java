eb1dd528ba141d2ce0f473b7423096436cd4a368
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2022-10-19 20:53:46 -0300

    Cliente Ok

--- LowTravels/src/main/java/br/low/travels/LowTravels/controller/ClienteController.java
+++ LowTravels/src/main/java/br/low/travels/LowTravels/controller/ClienteController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/editoralivros/cliente")
+@RequestMapping("/lowtravels/cliente")
 public class ClienteController {
     private ClienteService clienteService;
 
