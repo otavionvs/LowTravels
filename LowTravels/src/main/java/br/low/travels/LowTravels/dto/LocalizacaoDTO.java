3e8405564a56bbd2150cb05c0ca40f6f4d004674
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2022-10-19 21:18:00 -0300

    Transporte Ok

--- LowTravels/src/main/java/br/low/travels/LowTravels/dto/LocalizacaoDTO.java
+++ LowTravels/src/main/java/br/low/travels/LowTravels/dto/LocalizacaoDTO.java
@@ -1,5 +1,10 @@
 package br.low.travels.LowTravels.dto;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class LocalizacaoDTO {
     private String tipo;
     private String nome;
